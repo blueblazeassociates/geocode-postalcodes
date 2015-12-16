2ea8474afed57a86b52c3bb1e922255ff16e2bb8
Ed Gifford <egifford@blueblazeassociates.com>
2015-12-16 17:17:04 -0500

    Initial version of CountryCodes. Only United States is supported for now.

---
+++ src/php/CountryCodes.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace BlueBlazeAssociates\Geocoding;
+
+/**
+ * ISO 3166-1 alpha-2
+ *
+ * https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements
+ *
+ * Only the United States is supported for now.
+ *
+ * @author Ed Gifford
+ */
+abstract class CountryCodes {
+  /**
+   * United States of America
+   *
+   * @var string
+   */
+  const US = 'US';
+}
